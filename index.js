7384039ff8d58fbb3ab5c6722361dce53050a1e0
sarvottam <kumarpurush2511@gmail.com>
2024-05-10 13:25:54 +0530

    add some more routes

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const PORT = 3000;
 app.get('/', (req , res)=>{
     res.send("Hello Home page !");
 });
+app.get('/me',(req , res)=>{
+    res.send('<h1>Its Sarvottam </h1>');
+});
+app.get('/you', (req, res)=>{
+    res.send('<h1> How are you</h1>')
+});
 
 app.listen(process.env.PORT || PORT , ()=>{
     console.log(`Server is running on ${PORT}`)
